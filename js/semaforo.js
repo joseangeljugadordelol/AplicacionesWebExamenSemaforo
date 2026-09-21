13e78229ed63af44d651183d337914c8aafc5bbd
joseangeljugadordelol <joseangel.com.com@gmail.com>
2026-09-20 20:44:17 -0700

    este comit debio ser muchos comits chiquitos pero es demaciado largo, primero se creo un html donde estan todas las cosas que vamos a utilizar tanto los botones como los contadores y el semaforo, luego en el js se declararon las costancias de los botones y cuando se activan las funciones estan los text fild de los contadores junto a contadores, tambien estan las invocaciones al styles para llamar a una lista de clases para poder encender y apagar las lucecitas

---
+++ js/semaforo.js
@@ -0,0 +1,71 @@
+const botonrojo = document.getElementById("btnrojo");
+const botonamarrillo = document.getElementById("btnamarrillo");
+const botonverde = document.getElementById("btnverde");
+const botonmisterioso= document.getElementById("btnmisterioso");
+
+let acumuladorrojo = 0;
+let acumuladoramarrillo = 0;
+let acumuladorverde = 0;
+let acumuladormisterioso = 2;
+
+const diosito = document.querySelector(".luz");
+
+const luzroja = document.querySelector(".luz.rojo");
+const luzamarilla = document.querySelector(".luz.amarillo");
+const luzverde = document.querySelector(".luz.verde");
+
+botonrojo.addEventListener("click", function(){
+
+    const rojo = document.getElementById("tfrojo");
+
+    acumuladorrojo= acumuladorrojo + 1;
+
+    rojo.textContent = acumuladorrojo;
+    acumuladormisterioso = 3;
+
+    luzroja.classList.add("encendida");
+    luzamarilla.classList.remove("encendida");
+    luzverde.classList.remove("encendida");
+    
+})
+botonamarrillo.addEventListener("click", function(){
+
+    const amarrillo = document.getElementById("tfamarrillo");
+
+    acumuladoramarrillo= acumuladoramarrillo + 1;
+
+    amarrillo.textContent =  acumuladoramarrillo;
+    acumuladormisterioso = 2;
+
+    luzroja.classList.remove("encendida");
+    luzamarilla.classList.add("encendida");
+    luzverde.classList.remove("encendida");
+})
+botonverde.addEventListener("click", function(){
+
+    const verde = document.getElementById("tfverde");
+
+    acumuladorverde= acumuladorverde + 1;
+
+    verde.textContent = acumuladorverde;
+    acumuladormisterioso = 1;
+
+    luzroja.classList.remove("encendida");
+    luzamarilla.classList.remove("encendida");
+    luzverde.classList.add("encendida"); 
+})
+botonmisterioso.addEventListener("click", function(){
+    if (acumuladormisterioso==3){
+        botonverde.click();
+        return
+    }
+    if (acumuladormisterioso==2){
+        botonrojo.click();
+        return
+    }
+    if (acumuladormisterioso==1){
+        botonamarrillo.click();
+        return
+    }
+
+})
